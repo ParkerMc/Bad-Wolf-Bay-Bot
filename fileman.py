b6992fe4ce38743618afc954ef1cb62bfb2474e3
ParkerMc <ParkerMc@users.noreply.github.com>
2017-04-07 20:02:43 -0500

    Update fileman.py

--- fileman.py
+++ fileman.py
@@ -7,19 +7,18 @@ import os
 
 class fileman:
     def __init__(self, filename):
+        self.filename = filename
         if os.path.isfile(filename):
             self.newfile = False
             self.f = open(filename, "r")
             self.text = self.f.readlines()
             self.f.close()
-            self.f = open(filename, "w")
         else:
             self.newfile = True
-            self.f = open(filename, "w")
             self.text = []
         
-    def text(self):        
-        return "\n".join()
+    def toText(self):        
+        return "\n".join(self.text)
     
     def toArray(self):
         return self.text
@@ -27,8 +26,11 @@ class fileman:
     def toT2Array(self):
         out = []
         for i in self.text:
-            out.append((i.split(",")[0],i.split(",")[1]))
+            out.append((i.split(",")[0].replace("\n", ""),i.split(",")[1].replace("\n", "")))
         return out
     
     def save(self, text):
-        self.f.writelines(text)
+        self.f = open(self.filename, "w")
+        for i in text:
+            self.f.write(i)
+        self.f.close()
