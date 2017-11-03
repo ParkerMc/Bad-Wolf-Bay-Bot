ab7ff414c477ed888f81740cab6f67b58d695758
ParkerMc <ParkerMc@users.noreply.github.com>
2017-11-02 21:34:27 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
 module.exports = {
   roles: ["imps", "spawn", "sirens", "hydra heads", "leviathan", "kraken"],
-  description: "An bot for RR use."
+  description: "An bot for RR use.",
+  serverId: "270055309786087435"
 };
